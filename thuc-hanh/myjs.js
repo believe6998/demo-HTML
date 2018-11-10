fbe82cd1de9afe924b04b6345838a8c9ac3f6dd6
hoainam <namphth1807043@fpt.edu.vn>
2018-11-10 17:50:15 +0700

    nop-thuc-hanh

--- thuc-hanh/myjs.js
+++ thuc-hanh/myjs.js
@@ -10,21 +10,26 @@ var btnSubmit = document.forms['register-form']['btn-submit'];
             msgError.innerHTML = "* Tên bạn quá dài.";
             msgError.classList.remove("hidden-text");
         } else {
-            msgError.innerHTML = "Ten hop le.";
+            msgError.innerHTML = "* Tên hợp lệ.";
             msgError.classList.remove("hidden-text");
             msgError.classList.remove("danger-text");
             msgError.classList.add("success-text");
         }
-        var txtEmail = document.forms["register-form"]["email"];
-        var msgEmail = txtEmail.nextElementSibling;
-        var aCong = txtEmail.value.indexOf("@");
-        if (txtEmail == null || txtEmail.value.length == 0) {
-            msgEmail.innerHTML = "* Vui long nhap email.";
+        var txtemail = document.forms['register-form']['email'];
+        var msgEmail = txtemail.nextElementSibling;
+        var aCong=txtemail.value.indexOf("@");
+        if (txtemail == null || txtemail.value.length == 0) {
+            msgEmail.innerHTML = "* Vui lòng nhập email.";
             msgEmail.classList.remove("hidden-text");
         }
-        else if (aCong < 1) {
-            msgEmail.innerHTML = "* Email phai co @.";
+        else if (aCong<1) {
+            msgEmail.innerHTML = "* Email phải có @.";
             msgEmail.classList.remove("hidden-text");
+        }else {
+            msgEmail.innerHTML = "* Email hợp lệ.";
+            msgEmail.classList.remove("hidden-text");
+            msgEmail.classList.remove("danger-text");
+            msgEmail.classList.add("success-text");
         }
         var txtPhone = document.forms["register-form"]["phone"];
         var msgPhone = txtPhone.nextElementSibling;
@@ -33,14 +38,14 @@ var btnSubmit = document.forms['register-form']['btn-submit'];
             msgPhone.classList.remove("hidden-text");
         }
         else {
-            msgPhone.innerHTML = "Số diện thoại hop le.";
+            msgPhone.innerHTML = "Số diện thoại hợp lệ.";
             msgPhone.classList.remove("hidden-text");
             msgPhone.classList.remove("danger-text");
             msgPhone.classList.add("success-text");
         }
         var Gender = document.forms["register-form"]["gender"];
         var msgGender = Gender.nextElementSibling;
-        if (Gender == null || Gender.value.length == 0) {
+        if (Gender.checked = false) {
             msgGender.innerHTML = "* Vui lòng chọn giới tính.";
             msgGender.classList.remove("hidden-text");
         }
